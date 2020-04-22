2eb5b9a0725b132f4ce637d2cb1aae79a5c475ef
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-04-22 12:41:16 -0300

    Se arregla la comparacion de insertion_sort

--- Ordenar lista/insertion.c
+++ Ordenar lista/insertion.c
@@ -5,10 +5,8 @@ void insertion_sort(GList* lista, FuncionComparadora compara){
   if(lista != NULL){
     for(GNodo *aux = lista->inicio;aux != NULL;aux=aux->sig)
       for(GNodo *aux2=lista->inicio;aux2 != aux;aux2=aux2->sig)
-        if(compara(aux2->dato, aux->dato) <= 0){
-          void* dato_aux = aux2->dato;
-          aux2->dato = aux->dato;
-          aux->dato = dato_aux;
+        if(compara(aux2->dato, aux->dato)){
+          swap_nodes(aux2->dato, aux->dato);
         }
   }
 }

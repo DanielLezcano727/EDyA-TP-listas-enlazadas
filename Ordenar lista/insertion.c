1043ef215df4791c8d86111dbcfdb4c62709c542
Daniel Lezcano <daniel.lezcano727@gmail.com>
2020-04-27 19:40:39 -0300

    Se realiza una version de insertion sort mas eficiente

--- Ordenar lista/insertion.c
+++ Ordenar lista/insertion.c
@@ -2,11 +2,28 @@
 #include <stdlib.h>
 
 void insertion_sort(GList* lista, FuncionComparadora compara){
-  if(lista != NULL){
-    for(GNodo *aux = lista->inicio;aux != NULL;aux=aux->sig)
-      for(GNodo *aux2=lista->inicio;aux2 != aux;aux2=aux2->sig)
-        if(compara(aux->dato, aux2->dato) < 0){
-          swap_nodes(aux, aux2);
-        }
+  if(lista != NULL && lista->inicio != NULL){
+    
+    GNodo *insertar = lista->inicio->sig;
+    lista->inicio->sig = NULL;
+    GNodo *anterior, *aux, *nodoInsertar;
+    while(insertar!=NULL){
+      anterior = NULL;
+      for(GNodo *listaAux=lista->inicio;listaAux != NULL && compara(listaAux->dato, insertar->dato) < 0; listaAux=listaAux->sig)
+        anterior = listaAux;
+
+      nodoInsertar = insertar;
+      insertar=insertar->sig;
+      
+      if(anterior==NULL){
+        aux = lista->inicio;
+        lista->inicio = nodoInsertar;
+        nodoInsertar->sig = aux;
+      }else{
+        aux = anterior->sig;
+        anterior->sig = nodoInsertar;
+        nodoInsertar->sig = aux;
+      }
+    }
   }
 }
